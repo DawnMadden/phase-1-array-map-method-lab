f2e3737ce44c54ae2380777c4a46e695a65d2e4b
Dawn Madden <coachdawnmadden@gmail.com>
2023-09-20 22:13:32 -0400

    done

--- index.js
+++ index.js
@@ -11,6 +11,49 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials
+// }
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(element){
+//In each element, capitalize the first letter of each word
+const wordSeparator =  element.split(' ');
+const stringSeparatingThing = wordSeparator.map(function(word){
+  return word[0].toUpperCase() + word.slice(1)
+  })
+  return stringSeparatingThing.join(' ')
+})
+
 }
+
+titleCased()
+
+
+
+// function titleCased() {
+//   // Access the global tutorials variable
+//   let tutorials = window.tutorials;
+
+//   // Create an empty array to store the title case tutorial names
+//   let titleCasedTutorials = [];
+
+//   // Loop through each tutorial name
+//   for (let i = 0; i < tutorials.length; i++) {
+//     let tutorial = tutorials[i];
+
+//     // Convert the tutorial name to title case
+//     let titleCasedTutorial = tutorial.toLowerCase().split(' ').map(function(word) {
+//       return (word.charAt(0).toUpperCase() + word.slice(1));
+//     }).join(' ');
+
+//     // Push the title case tutorial name into the new array
+//     titleCasedTutorials.push(titleCasedTutorial);
+//   }
+
+//   // Return the array with title case tutorial names
+//   return titleCasedTutorials;
+// }
+
+// // Test the titleCased function
+// console.log(titleCased());
